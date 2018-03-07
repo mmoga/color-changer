b985c4636f0f898786230358c7d7d3f196714a21
Matthew Mogavero <matt.moga@gmail.com>
2018-03-06 19:40:02 -0500

    initial implementation of store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,22 @@
 // types
-
+const CHANGE_COLOR = 'CHANGE_COLOR';
 // actions
-
+const changeColor = (color) => ({
+    type: CHANGE_COLOR,
+    payload: {
+        // tell it what to change
+        value: color
+    }
+});
 // initial state 
-
+const initialState = {
+    color: '#ffffff'
+}
 // reducer
-
+const reducer = (state=initialState, action) => {
+    return state;
+}
 // create store
-
-// 
+const store = createStore(reducer) //dev tools needs more args
+// export store
+export default store;
