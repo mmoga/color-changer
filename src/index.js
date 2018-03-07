8587e24264dabe09942ddb449435fa5433d02b9f
Matthew Mogavero <matt.moga@gmail.com>
2018-03-06 19:45:58 -0500

    edited comments in index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
 // import provider
+import { Provider } from 'react-redux';
 // import store
-// wrap app with provider (pass store to provider)
+import store from './store';
 
+// wrap app with provider (pass store to provider)
 ReactDOM.render(
     <Provider store={store}>
     <App />
